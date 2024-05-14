21e0c83c343a72ca08b7244d4d912999d89234c5
denis.badura <denis.badura@quorumsoftware.com>
2024-05-14 12:14:55 +0200

    fix mobile view

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,12 +3,12 @@
 export default function Footer() {
 
     return (
-        <div className="text-textsecondary absolute left-0 bottom-0">
+        <div className="text-textsecondary fixed left-0 bottom-0">
             Created by Denis
             @2024 Ostrava |||
             <span>   </span>
-            <span className="text-darkpurple pb-0 pointer-events-none">
-            Curious one huh? Good, I like you. And yes, even though I said I&apos;m working with Angular, this project was writen using Next.JS, because why not ¯\_(ツ)_/¯
+            <span className="text-darkpurple pb-0 pointer-events-none hidden lg:inline">
+            Curious one huh? Good, I like you. And yes, even though I said I&apos;m working with Angular, this project was written using Next.JS, because why not ¯\_(ツ)_/¯
         </span>
 
         </div>
--- src/app/components/list/list.tsx
+++ src/app/components/list/list.tsx
@@ -6,7 +6,7 @@ import Technology from './technology';
 
 export default function List() {
     return (
-        <div id={'skill'} className={'grid grid-cols-3 grid-flow-col gap-4 col-span-10 text-textrest page'}>            
+        <div id={'list'} className={'text-textrest page grid gap-4 mb-5 grid-rows-3 grid-flow-row row-span-10 md:grid-cols-3 md:grid-cols-row md:col-span-10'}>
             <Technology/>
             <Skill/>
             <Hobby/>
