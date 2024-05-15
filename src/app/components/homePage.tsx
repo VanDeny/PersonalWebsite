d4a45cd46111e8a366915a5d74fee1d038a6f76d
Denis Badura <badura.denis@gmail.com>
2024-05-15 18:18:12 +0200

    Added heading

--- src/app/components/homePage.tsx
+++ src/app/components/homePage.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
 
     return (
         <div id={'home'} className={'w-full pl-[3%]'}>
-            <div className={'text-textprimary pt-20'}>Denis Badura</div>
-            <div className={'text-textsecondary pt-2'}>least reliable developer in Ostrava</div>
+            <h1 className={'text-textprimary pt-20'}>Denis Badura</h1>
+            <h2 className={'text-textsecondary pt-2'}>least reliable developer in Ostrava</h2>
         </div>)
 }
