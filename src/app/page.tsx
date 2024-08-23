c2c42a42ee573b907fc92f069f46bb06e9ed588c
Denis Badura <badura.denis@gmail.com>
2024-08-23 12:55:30 +0200

    fix big icons, updated npm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+
 import List from './components/list/list'
 import About from './components/about/about'
 import HomePage from './components/homePage'
 import Footer from './components/footer'
+
+config.autoAddCss = false;
 
 export default function Home() {
   return (
