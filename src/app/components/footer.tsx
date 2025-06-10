a6df4086b590ba92df4a7886ffae56bf7bc8160c
denis.badura <denis.badura@quorumsoftware.com>
2025-06-10 08:42:50 +0200

    update

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <div className="text-textsecondary fixed left-0 bottom-0">
             Created by Denis
-            @2024 Ostrava |||
+            @2025 Ostrava |||
             <span>   </span>
             <span className="text-darkpurple pb-0 pointer-events-none hidden lg:inline">
             Curious one huh? Good, I like you. And yes, even though I said I&apos;m working with Angular, this project was written using Next.JS, because why not ¯\_(ツ)_/¯
