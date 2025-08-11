6bda0a637809aa73d7ce9a8d33b8a4fb8924744f
vandeny <badura.denis@gmail.com>
2025-08-11 20:19:15 +0200

    Updated packages, updated content

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -6,9 +6,10 @@ import {
     faThreads
   } from '@fortawesome/free-brands-svg-icons';
 
-const text = 'I\'m software developer based in Ostrava, Czech Republic, interested in web development, mountains, and good food. Currently I\'m working as a Software Developer under Quorum software, lending hand on Energy Components and it\'s extensions.';
+const text = 'I\'m software developer based in Ostrava, Czech Republic, interested in web development, mountains, and good food. Currently I\'m working as a Software Developer under Seznam.cz, lending hand on Sklik.';
 const freeToHire = 'I\'ll be free to work from August 1st 2025. If you are interested, you can contact me at ';
 const whereToFindMe = 'You can find me on numerous sites, try your luck finding me, or you can use shortcuts here '
+const imFree = false;
 
 export default function About() {
     return (
@@ -31,11 +32,13 @@ export default function About() {
                     <FontAwesomeIcon icon={faGithub}/>
                 </a>
             </p>
-            <p className='text-textrest pt-5'>{freeToHire}<a style={{color: '#6d4aff'}} href='mailto:badura.denis@pm.me'>{'e-mail'}</a> {', or on '}
-                <a style={{color: 'blue'}} href="https://linkedin.com/in/denis-badura">
-                    <FontAwesomeIcon icon={faLinkedinIn}/>
-                </a>
-            </p>
+            {imFree &&
+                <p className='text-textrest pt-5'>{freeToHire}<a style={{color: '#6d4aff'}} href='mailto:badura.denis@pm.me'>{'e-mail'}</a> {', or on '}
+                    <a style={{color: 'blue'}} href="https://linkedin.com/in/denis-badura">
+                        <FontAwesomeIcon icon={faLinkedinIn}/>
+                    </a>
+                </p>
+            }
         </div>
     )
 }
